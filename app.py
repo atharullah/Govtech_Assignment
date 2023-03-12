ff6f06da608f9e8a432f70e090d2fe7d58ea129a
Athar <Atharullah@NsearchGlobal.com>
2023-03-12 15:38:15 +0800

    formated dob column

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ def data_cleaning(df):
     # name column cleaning and spliting
     df[["first_name", "last_name"]]=df['name'].replace(['Miss ','Mr\. ','Ms\. ','Dr\. ','Mrs\. '],'', regex=True).str.split(' ', expand=True).iloc[:, :2]
     df=df.drop("name",axis=1)
+
+    #format dob
+    df["date_of_birth"]=pd.to_datetime(df['date_of_birth'], infer_datetime_format=True).dt.strftime('%Y%m%d')
+    
     print(df)
   
 
