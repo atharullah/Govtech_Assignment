deee05625afa32df23c5b59acfd4c37f7da4eee5
Athar <Atharullah@NsearchGlobal.com>
2023-03-12 15:12:05 +0800

    split name column

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+import pandas as pd
+import dateutil.parser
+
+def data_cleaning(df):
+    # name column cleaning and spliting
+    df[["first_name", "last_name"]]=df['name'].replace(['Miss ','Mr\. ','Ms\. ','Dr\. ','Mrs\. '],'', regex=True).str.split(' ', expand=True).iloc[:, :2]
+    df=df.drop("name",axis=1)
+    print(df)
+  
+
+if __name__ == "__main__":
+    # Data Ingestion
+    input_folder = 'input/'
+    output_folder = 'cleaned/'
+    for filename in os.listdir(input_folder):
+        if filename.startswith('applications_') and filename.endswith('.csv'):
+            input_path = os.path.join(input_folder, filename)
+            output_path = os.path.join(output_folder, filename)
+            df = pd.read_csv(input_path)
+            data_cleaning(df)
