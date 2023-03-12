9ef5d905a3bf725e6b4f23efc1d35283e77b7728
Athar <Atharullah@NsearchGlobal.com>
2023-03-12 18:07:01 +0800

    Added database scripts, container and required queries file

--- dag.py
+++ dag.py
@@ -4,6 +4,11 @@ from airflow.operators.python_operator import PythonOperator
 import pandas as pd
 import os
 import hashlib
+from scripts.data_cleaning_and_validation import data_cleaning_and_validation
+
+from airflow.providers.docker.operators.docker import DockerOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.sensors.sql import SqlSensor
 
 # define default arguments
 default_args = {
@@ -23,59 +28,38 @@ dag = DAG(
     catchup=False
 )
 
-# define function to process membership applications
-def process_membership_applications(**context):
-    input_folder = 'input/'
-    success_folder = 'success/'
-    failed_folder = 'failed/'
-    
-    for filename in os.listdir(input_folder):
-        if filename.startswith('applications_') and filename.endswith('.csv'):
-            input_path = os.path.join(input_folder, filename)
-            success_path = os.path.join(success_folder, filename)
-            failed_path = os.path.join(failed_folder, filename)
-            df = pd.read_csv(input_path)
-
-            # Remove any rows which do not have a name field (treat this as unsuccessful applications)
-            failed_df=df[df['name'].isnull()]
-            df = df.dropna(subset=['name'])
-
-            # Split name into first_name and last_name
-            df[["first_name", "last_name"]]=df['name'].replace(['Miss ','Mr\. ','Ms\. ','Dr\. ','Mrs\. '],'', regex=True).str.split(' ', expand=True).iloc[:, :2]
-            df=df.drop("name",axis=1)
-
-            # Format birthday field into YYYYMMDD
-            df["date_of_birth"]=pd.to_datetime(df['date_of_birth'], infer_datetime_format=True).dt.strftime('%Y%m%d')
-
-            # Create above_18 column to check if applicant is over 18 years old as of 1 Jan 2022
-            df['above_18'] = pd.to_datetime(df['date_of_birth']).apply(lambda x: (pd.Timestamp('2022-01-01') - x).days // 365 >= 18)
-            df['above_18'] = df['above_18'].astype(bool)
-
-            # Check if mobile number is 8 digits
-            failed_df=pd.concat([failed_df,df[df['mobile_no'].astype(str).str.len() != 8]])
-            df = df[df['mobile_no'].astype(str).str.len() == 8]
-
-            # Check if applicant is over 18 years old as of 1 Jan 2022
-            failed_df=pd.concat([failed_df,df[~df['above_18']]])
-            df = df[df['above_18']]
-
-            # Check if applicant has a valid email
-            email_pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-            failed_df=pd.concat([failed_df,df[~df['email'].str.contains(email_pattern)]])
-            df = df[df['email'].str.contains(email_pattern)]
-
-            #Create membership id for successfull candidates
-            df['membership_id'] = df.apply(lambda row: f"{row['last_name']}_{hashlib.sha256(row['date_of_birth'].encode('utf-8')).hexdigest()[:5]}", axis=1)
-            failed_df.to_csv(failed_path, index=False)
-            df.to_csv(success_path, index=False)
-
 # define task
 process_applications_task = PythonOperator(
     task_id='process_applications',
-    python_callable=process_membership_applications,
+    python_callable=data_cleaning_and_validation,
     provide_context=True,
     dag=dag
 )
 
+create_database_container = DockerOperator(
+    task_id='create_database_container',
+    image='postgres',
+    dockerfile='govtech/database/Dockerfile',
+    command='postgres -c logging_collector=on',
+    network_mode='bridge',
+    auto_remove=True,
+    tty=True,
+    dag=dag,
+)
+
+initialize_database = PostgresOperator(
+    task_id='initialize_database',
+    postgres_conn_id='postgres_default',
+    sql='govtech/database/init.sql',
+    dag=dag,
+)
+
+wait_for_database = SqlSensor(
+    task_id='wait_for_database',
+    conn_id='postgres_default',
+    sql='SELECT 1',
+    dag=dag,
+)
+
 # set task dependencies
-process_applications_task
+process_applications_task >> create_database_container
