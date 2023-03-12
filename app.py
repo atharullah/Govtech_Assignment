f32d986a0d64f5f17c54ffaf314c746a20c4c9cf
Athar <Atharullah@NsearchGlobal.com>
2023-03-12 15:54:10 +0800

    Remove any rows which do not have a name field (treat this as unsuccessful applications)

--- app.py
+++ app.py
@@ -3,11 +3,15 @@ import pandas as pd
 import dateutil.parser
 
 def data_cleaning(df):
-    # name column cleaning and spliting
+    # Remove any rows which do not have a name field (treat this as unsuccessful applications)
+    unssuccessfull_df=df[df['name'].isnull()]
+    df = df.dropna(subset=['name'])
+
+    # Split name into first_name and last_name
     df[["first_name", "last_name"]]=df['name'].replace(['Miss ','Mr\. ','Ms\. ','Dr\. ','Mrs\. '],'', regex=True).str.split(' ', expand=True).iloc[:, :2]
     df=df.drop("name",axis=1)
 
-    #format dob
+    # Format birthday field into YYYYMMDD
     df["date_of_birth"]=pd.to_datetime(df['date_of_birth'], infer_datetime_format=True).dt.strftime('%Y%m%d')
     
     print(df)
